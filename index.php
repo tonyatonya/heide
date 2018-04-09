d56afdc05415fa84bbbb210f267a3157254ed319
Athapong Athamasirigun <tonya_0@hotmail.com>
2018-04-09 14:56:17 +0700

    first Install Website

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Bootstrap 101 Template</title>
+<?php include('inc_head.php'); ?>
+</head>
+<body>
+	<?php include('inc_header.php'); ?>	
+	<!-- content here-->
+	<!-- end content here -->
+	<?php include('inc_footer.php'); ?>
+</body>
+</html>
