5e3ec9bfecc424a6be77a74b6e704f088558b930
Athapong Athamasirigun <tonya_0@hotmail.com>
2018-04-15 15:34:31 +0700

    fix bugs header effect
    Change from toggle header class to change css property by jQuery

--- js/main.js
+++ js/main.js
@@ -16,9 +16,60 @@ $(function(){
 	$(window).scroll(function (event) {
 		var scroll = $(window).scrollTop();
 		if((scroll > currPos) && (scroll > 0) && (scroll < $(".wrapper").height())){
-			$(".wrapper").addClass("header-small");
+			/*
+			if($(".wrapper").hasClass("header-small") == false){
+				$(".wrapper").addClass("header-small");
+			}
+			*/
+
+			$(".main-logo img").css({
+				'transform':'scale(0.74)'
+			})
+			$(".section-menu").css({
+				'transform':'scale(0,0)',
+				'height': 0,
+				'transform': 'translate(0,-100%)',
+				'opacity': 0,
+				'overflow': 'hidden'
+			})
+
+			$(".hamburger-bar").css({
+				'opacity': 1
+			})
+
+			$(".header-desktop hr").css({
+				'margin-top': 0,
+				'opacity': 0,
+			});
+
 		}else{
-			$(".wrapper").removeClass("header-small");
+
+			$(".main-logo img").css({
+				'transform':'scale(1)'
+			})
+
+			$(".section-menu").css({
+				'transform':'scale(1,1)',
+				'height': 'auto',
+				'transform': 'translate(0,0)',
+				'opacity': 1,
+				'overflow': 'visible'
+			})
+			$(".hamburger-bar").css({
+				'opacity': 0
+			})
+
+			$(".header-desktop hr").css({
+				'margin-top': 20,
+				'opacity': 1,
+			});
+
+
+			/*
+			if($(".wrapper").hasClass("header-small") == true){
+				$(".wrapper").removeClass("header-small");
+			}
+			*/
 		}
 		currPos = scroll;
 	})
